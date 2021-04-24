311036ec858ac6c7b75deab10e7a814bd0cc95b2
NavPoint1 <foleym156@gmail.com>
2021-04-24 18:56:11 -0400

    toggle logic

--- src/containers/ToggleContainer.js
+++ src/containers/ToggleContainer.js
@@ -2,31 +2,57 @@ import React from 'react'
 import Button from '@material-ui/core/Button';
 
 const ToggleContainer = (props) => {
+
+    const handleClick = (toggle) => {
+        switch(toggle) {
+            case "acres": 
+                props.setAcresToggle(!props.acresToggle)
+                if (props.durationToggle) {
+                    props.setDurationToggle(!props.durationToggle)
+                }
+                break
+            case "duration":
+                props.setDurationToggle(!props.durationToggle)
+                if (props.acresToggle) {
+                    props.setAcresToggle(!props.acresToggle)
+                }
+                break
+            case "cause":
+                props.setCauseToggle(!props.causeToggle)
+                break
+            case "ongoing":
+                props.setOngoingToggle(!props.ongoingToggle)
+                break
+            default:
+                break
+        }
+    }
+
     return (
         <div className="map-element" id="toggle-container">
             <Button 
-                onClick={() => props.setAcresToggle(!props.acresToggle)}
+                onClick={() => handleClick("acres")}
                 color="secondary" 
                 variant={props.acresToggle ? "contained" : "outlined"}
                 className='map-filter-button'>
                     Acres
             </Button>
             <Button 
-                onClick={() => props.setDurationToggle(!props.durationToggle)}
+                onClick={() => handleClick("duration")}
                 color="secondary" 
                 variant={props.durationToggle ? "contained" : "outlined"} 
                 className='map-filter-button'>
                     Duration
             </Button>
             <Button 
-                onClick={() => props.setCauseToggle(!props.causeToggle)}
+                onClick={() => handleClick("cause")}
                 color="secondary" 
                 variant={props.causeToggle ? "contained" : "outlined"}
                 className='map-filter-button'>
                     Cause
             </Button>
             <Button 
-                onClick={() => props.setOngoingToggle(!props.ongoingToggle)}
+                onClick={() => handleClick("ongoing")}
                 color="secondary" 
                 variant={props.ongoingToggle ? "contained" : "outlined"} 
                 className='map-filter-button'>
