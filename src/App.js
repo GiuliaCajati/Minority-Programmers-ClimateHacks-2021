ae927cfbc11dbc59a946815159013d72137eec87
aysikh <amy.sikhammountry@gmail.com>
2021-04-24 19:23:27 -0400

    popover works

--- src/App.js
+++ src/App.js
@@ -2,13 +2,23 @@ import './App.css';
 import { useEffect, useState } from 'react'
 import MapPage from './containers/MapPage.js'
 import GraphContainer from './containers/GraphContainer.js'
-import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import PieChart from './components/Piechart.js'
 
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import { makeStyles } from "@material-ui/core/styles";
 
 let url = 'https://services3.arcgis.com/T4QMspbfLg3qTGWY/arcgis/rest/services/Fire_History_Perimeters_Public/FeatureServer/0/query?where=1%3D1&outFields=OBJECTID,poly_IncidentName,irwin_DailyAcres,irwin_FireBehaviorGeneral1,irwin_FireCause,irwin_FireCauseGeneral,irwin_FireCauseSpecific,irwin_FireDiscoveryDateTime,irwin_FireMgmtComplexity,irwin_FireOutDateTime,irwin_IncidentTypeCategory,irwin_InitialLatitude,irwin_InitialLongitude,irwin_POOState,irwin_PredominantFuelGroup,irwin_SecondaryFuelModel,irwin_TotalIncidentPersonnel,poly_Acres_AutoCalc,irwin_FireBehaviorGeneral2,irwin_FireBehaviorGeneral3,irwin_POOCounty,irwin_POOLandownerKind,irwin_PrimaryFuelModel&returnGeometry=false&outSR=4326&f=json'
 
-function App() {
+const useStyles = makeStyles(theme => ({
+  hide: {
+    opacity: 0, 
+    visibility: "hidden", 
+    zIndex: -1
+  }
+}))
 
+function App() {
+  const classes = useStyles()
   const [ data, setData ] = useState([])
 
   useEffect(() => {
@@ -49,18 +59,27 @@ function App() {
   }
 
 
+
   return (
     <Router>
       <div className="App">
         {/* <Switch>
-          <Route path='/map' render ={() => <MapPage fireData = {data} />}/>
-          <Route path='/graphs' render = {() => <GraphContainer fireData = {data}/>}/>
-          <Redirect to='/map' />
-        </Switch> */}
+          <Route exact path="/map" transitionProps={{...hideInsteadOfUnmount}} render={() => 
+            <MapPage fireData={data}/>
+          }/> 
+          {/* <Route exact path='/map' render ={() => <MapPage fireData = {data} />}/> */}
+          {/* <Route exact path='/graphs' render ={() => 
+          }/>  */}
+          {/* <Route exact path="/pie" render={() =>
+            <PieChart fireData={data}/> 
+          } /> */}
+        {/* </Switch> */} 
         <MapPage fireData = {data} />
+        {/* <GraphContainer data={data}/>  */}
       </div>
     </Router>
   );
 }
 
 export default App;
+
--- src/containers/GraphContainer.js
+++ src/containers/GraphContainer.js
@@ -1,11 +1,15 @@
 import React, {useState} from 'react'
 import Piechart from '../components/Piechart'
-
+import Paper from '@material-ui/core/Paper';
 const GraphContainer = (props) => {
 
     return (
         <div id="graph-page">
+          {/* <Paper> */}
+          testing 
+
             {props.data ? <Piechart data={props.data}/> : null}
+          {/* </Paper> */}
         </div>
     )
 }
