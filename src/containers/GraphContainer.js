6fdecc9c9b750b71452d8eda6c6fb6eb2c914649
KevJSDevelopment <Kshhn12@gmail.com>
2021-04-24 19:28:47 -0400

    added grid to graphs

--- src/containers/GraphContainer.js
+++ src/containers/GraphContainer.js
@@ -1,11 +1,28 @@
 import React, {useState} from 'react'
 import Piechart from '../components/Piechart'
+import { getAverageAcres, getTotalFires, renderCustomizedLabel} from '../helpers/piedata'
+import { Grid } from '@material-ui/core'
 
-const GraphContainer = (props) => {
+const GraphContainer = ({data}) => {
 
     return (
         <div id="graph-page">
-            {props.data ? <Piechart data={props.data}/> : null}
+            {data ? 
+                <Grid container className="graph-container">
+                    <Grid item xs={3}>
+                        <Piechart data={getAverageAcres(data)} /> 
+                    </Grid>
+                    <Grid item xs={3}>
+                        <Piechart data={getTotalFires(data)} />
+                    </Grid>
+                    <Grid item xs={3}>
+                        
+                    </Grid>
+                    <Grid item xs={3}>
+                        
+                    </Grid>
+                </Grid>
+            : null}
         </div>
     )
 }
